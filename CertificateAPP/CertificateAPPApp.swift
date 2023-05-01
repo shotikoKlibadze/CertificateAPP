1718a3b355b7f94d74ee3c82c352c0e59c949b79
Shota <sh.klibadze@gmail.com>
2023-05-01 17:01:20 +0400

    Initial Commit

---
+++ CertificateAPP/CertificateAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CertificateAPPApp.swift
+//  CertificateAPP
+//
+//  Created by Shotiko Klibadze on 01.05.23.
+//
+
+import SwiftUI
+
+@main
+struct CertificateAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
